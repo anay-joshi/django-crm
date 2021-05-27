9e2c6879c5ffc910ed0bf703fb9c1816b7710317
Anay Joshi <anayjoshi1608@gmail.com>
2021-05-27 15:20:56 +0530

    Password Reset functionality added

--- django_crm/django_crm/settings.py
+++ django_crm/django_crm/settings.py
@@ -135,3 +135,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'aladin01012000@gmail.com'
+EMAIL_HOST_PASSWORD = 'Aladin@112000'
