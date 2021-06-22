7fe611a05dcece7dad594e1ff9985740d221cc47
Anay Joshi <anayjoshi1608@gmail.com>
2021-06-22 22:55:06 +0530

    heroku-setup added

---
+++ django_crm/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn django_crm.wsgi --log-file -
--- django_crm/django_crm/settings.py
+++ django_crm/django_crm/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%!q9k24f^#ui&aysg5=bv=5i!1mh9f@o10s!7jtkwn!2&=$to$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['curem-web.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
