1d71d7515f781ef12e5edd07148b79e77d09ac7a
Anay Joshi <anayjoshi1608@gmail.com>
2021-07-16 12:21:46 +0530

    Finishing up with the settings and env files

--- django_crm/settings.py
+++ django_crm/settings.py
@@ -129,8 +129,12 @@ USE_TZ = True
 
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
